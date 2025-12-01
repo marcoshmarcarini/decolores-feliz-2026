6dbea786a51951c548c117671b969a5d21f166b0
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2025-12-01 16:10:37 -0300

    Update

---
+++ app/en/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+
+export default function En() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const redirectToYoutube = () =>{
+      router.push("https://www.youtube.com/watch?v=7_9s4fbtBRM");
+    }
+    redirectToYoutube();
+  }, [router]);
+  return (
+    <></>
+  );
+}
