b64e5c9b726a626faa562d62d525bebf6e11006b
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2025-12-11 10:32:23 -0300

    Update
    Subi o video de 2025 para acessá-lo antes do video de 2026 ficar pronto.

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,29 @@
-'use client';
+"use client";
 
-
-import { useRouter } from "next/navigation";
+//import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-
 export default function Home() {
-  const router = useRouter();
+  /* const router = useRouter();
 
   useEffect(() => {
     const redirectToYoutube = () =>{
       router.push("https://www.youtube.com/watch?v=7_9s4fbtBRM");
     }
     redirectToYoutube();
-  }, [router]);
+  }, [router]); */
+
+  /* Video menor que 100mb */
+
   return (
-    <></>
+    <>
+      <div className={`bg-green-900 flex items-center justify-center h-screen`}>
+        <video
+          src="/videos/decolores_fim_de_ano_2025.mp4"
+          autoPlay
+          controls
+        ></video>
+      </div>
+    </>
   );
 }
