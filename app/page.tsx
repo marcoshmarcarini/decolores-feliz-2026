d2b1fea306227d9023281dee64a1f34e84dad756
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2025-12-15 09:53:34 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,29 @@
 
 //import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  /* const router = useRouter();
+  const router = useRouter();
 
   useEffect(() => {
     const redirectToYoutube = () =>{
-      router.push("https://www.youtube.com/watch?v=7_9s4fbtBRM");
+      router.push("https://drive.google.com/file/d/1foR5gRIEEQrOEo0O9siBKyFnklFD2-1k/view?usp=sharing");
     }
     redirectToYoutube();
-  }, [router]); */
+  }, [router]);
 
   /* Video menor que 100mb */
 
   return (
     <>
-      <div className={`bg-green-900 flex items-center justify-center h-screen`}>
+      {/* <div className={`bg-green-900 flex items-center justify-center h-screen`}>
         <video
           src="/videos/decolores_fim_de_ano_2025.mp4"
           autoPlay
           controls
         ></video>
-      </div>
+      </div> */}
     </>
   );
 }
